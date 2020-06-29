02a826e45876dc2769591eead5fe0b3d6a9f14d7
Quentin Kniep <hello@quentinkniep.com>
2020-06-29 18:54:36 +0200

    stuff

--- FlatFAT.py
+++ FlatFAT.py
@@ -24,9 +24,39 @@ class FlatFAT:
                     next_level_dirty.add(parent(i))
             dirty = next_level_dirty
 
+    # TODO: remove this (see ra.py)
+    def find_leaf(self, value):
+        """."""
+        for i in range(self.capacity()):
+            if self.data[self.leaf(i)] == value:
+                return i
+        return -1
+
     def aggregate(self):
         """Returns the current root-level partial aggregate."""
         return self.data[0]
+
+    def prefix(self, index):
+        """."""
+        node = self.leaf(index)
+        agg = self.data[node]
+        while node != 0:
+            p = parent(node)
+            if node == right(p):
+                agg = self.combine_values(self.data[left(p)], agg)
+            node = p
+        return agg
+
+    def suffix(self, index):
+        """."""
+        node = self.leaf(index)
+        agg = self.data[node]
+        while node != 0:
+            p = parent(node)
+            if node == left(p):
+                agg = self.combine_values(agg, self.data[right(p)])
+            node = p
+        return agg
 
     def resize(self, new_capacity):
         """."""
@@ -36,14 +66,24 @@ class FlatFAT:
 
     def compact(self):
         """."""
-        # changes = []
-        # for i in range(leaf(0), len(self.data)-1):
-        #     if self.data[i] is None:
+        changes = []
+        shift_count = 0
+        for i in range(self.capacity()):
+            if self.data[self.leaf(i)] is None:
+                shift_count += 1
+            else:
+                changes.append((i-shift_count, self.data[self.leaf(i)]))
+        last_value_index = self.capacity() - shift_count
+        changes += [(last_value_index+i, None) for i in range(shift_count)]
+        self.update(changes)
 
     def combine(self, index1, index2):
         """."""
         agg1 = self.data[index1]
         agg2 = self.data[index2]
+        return self.combine_values(agg1, agg2)
+
+    def combine_values(self, agg1, agg2):
         if agg1 is None:
             return agg2
         if agg2 is None:
